69b644e1374ac75eb279abb26bc5843561fb956d
Alexander Ignatov <estaxo@yandex.ru>
2022-07-29 23:43:33 +0300

    Update Portal.gd

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -9,8 +9,8 @@ onready var drag_point = player.get_node("RotationHelper/DragRay/DragPoint")
 
 onready var portal_plane = $"PortalPlane"
 onready var portal_area = $"PortalPlane/PortalArea"
-onready var portal_camera_dummy = $"PortalPlane/PortalCameraDummy"
-onready var portal_player_dummy = $"PortalPlane/PortalPlayerDummy"
+onready var camera_dummy = $"PortalPlane/PortalCameraDummy"
+onready var player_dummy = $"PortalPlane/PortalPlayerDummy"
 
 onready var portal_target = $"PortalTarget"
 onready var portal_camera = $"PortalTarget/PortalCamera"
@@ -26,17 +26,17 @@ func _ready():
 
 func _process(_delta):
 	# Orient camera in space
-	portal_camera_dummy.global_transform.origin = player_camera.global_transform.origin
-	portal_camera_dummy.global_transform.basis = player_camera.global_transform.basis
-	portal_camera.transform.origin = portal_camera_dummy.transform.origin
-	portal_camera.transform.basis = portal_camera_dummy.transform.basis
+	camera_dummy.global_transform.origin = player_camera.global_transform.origin
+	camera_dummy.global_transform.basis = player_camera.global_transform.basis
+	portal_camera.transform.origin = camera_dummy.transform.origin
+	portal_camera.transform.basis = camera_dummy.transform.basis
 	
-	portal_player_dummy.global_transform.origin = player.global_transform.origin
-	portal_player_dummy.global_transform.basis = player.global_transform.basis
+	player_dummy.global_transform.origin = player.global_transform.origin
+	player_dummy.global_transform.basis = player.global_transform.basis
 	
 	# Check distance to player, do not render if far
 	player_distance = portal_plane.global_transform.origin.distance_to(player.global_transform.origin)
-	player_offset = portal_player_dummy.global_transform.origin - portal_plane.global_transform.origin
+	player_offset = player_dummy.global_transform.origin - portal_plane.global_transform.origin
 	
 	if player_offset.length() < 5:
 		render_target_update_mode = Viewport.UPDATE_ALWAYS
@@ -55,7 +55,7 @@ func _process(_delta):
 func _on_body_exited(body):
 	if body.name == "Player":
 		
-		player_offset = portal_player_dummy.global_transform.origin - portal_plane.global_transform.origin
+		player_offset = player_dummy.global_transform.origin - portal_plane.global_transform.origin
 		
 		if player_offset.dot(portal_plane.transform.basis.z) < 0:
 			
